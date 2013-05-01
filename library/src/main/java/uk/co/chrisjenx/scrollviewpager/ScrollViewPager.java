15a09bcf403368f3fb8ffedc5c016d0c8f3441c1
Chris Jenkins <chris.mark.jenkins@gmail.com>
2013-05-01 01:40:46 +0100

    huge improvements

--- library/src/main/java/uk/co/chrisjenx/scrollviewpager/ScrollViewPager.java
+++ library/src/main/java/uk/co/chrisjenx/scrollviewpager/ScrollViewPager.java
@@ -1,8 +1,10 @@
 package uk.co.chrisjenx.scrollviewpager;
 
+import android.support.v4.view.VelocityTrackerCompat;
+import android.util.FloatMath;
 import android.util.Log;
 import android.view.*;
-import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.ScrollView;
 import android.widget.Scroller;
 
@@ -36,7 +38,7 @@ public class ScrollViewPager implements View.OnTouchListener
         mContentView = contentView;
         // Setup
         mScrollView.setOnTouchListener(this);
-        mScroller = new Scroller(mScrollView.getContext(), new AccelerateDecelerateInterpolator());
+        mScroller = new Scroller(mScrollView.getContext(), new DecelerateInterpolator());
         task = new Runnable()
         {
             @Override
@@ -45,7 +47,6 @@ public class ScrollViewPager implements View.OnTouchListener
                 if (mScroller.computeScrollOffset())
                 {
                     mScrollView.scrollTo(mScroller.getCurrX(), mScroller.getCurrY());
-                    mScrollView.postInvalidate();
                 }
                 if (!mScroller.isFinished())
                 {
@@ -69,7 +70,9 @@ public class ScrollViewPager implements View.OnTouchListener
             mVelocityTracker = VelocityTracker.obtain();
         mVelocityTracker.addMovement(ev);
 
-        final int action = ev.getAction();
+        final int index = ev.getActionIndex();
+        final int action = ev.getActionMasked();
+        final int pointerId = ev.getPointerId(index);
         switch (action)
         {
             case MotionEvent.ACTION_DOWN:
@@ -91,7 +94,7 @@ public class ScrollViewPager implements View.OnTouchListener
                 //Calculate current velocity
                 final VelocityTracker velocityTracker = mVelocityTracker;
                 velocityTracker.computeCurrentVelocity(1000, mMaxVelocity);
-                final float velocityY = velocityTracker.getYVelocity();
+                final float velocityY = VelocityTrackerCompat.getYVelocity(velocityTracker, pointerId);
                 if (DEBUG) Log.d(TAG, String.format("VelocityY [%f]", velocityY));
 
 
@@ -106,15 +109,17 @@ public class ScrollViewPager implements View.OnTouchListener
 
                 // Check not scrolling past first/last page
                 if (getCurrentScrollY() < 0)
-                    snapToPage(0);
+                    snapToPage(0, 0);
                 else if (getCurrentScrollY() > lastPageTopY)
-                    snapToPage(pageCount - 1);
+                    snapToPage(pageCount - 1, 0);
                 else
                 {
                     // The scrolled top position of scroll view, in pixels.
                     final int currScrollY = getCurrentScrollY();
                     // The scrolled middle position of scroll view, in pixels.
                     final int currScrollMiddleY = currScrollY + displayHeight / 2;
+                    if (DEBUG)
+                        Log.d(TAG, String.format("ScrollY [%d], ScrollMiddleY [%d]", currScrollY, currScrollMiddleY));
 
                     // Current page num.
                     final int currPage = currScrollMiddleY / displayHeight;
@@ -125,14 +130,24 @@ public class ScrollViewPager implements View.OnTouchListener
                     // The top of next page, in pixels.
                     final int nextPageTop = nextPage * displayHeight;
 
+                    if (DEBUG) Log.d(TAG, String.format("NextPageTop [%d]", nextPageTop));
+                    if (Math.abs(velocityY) >= mMinVelocity)
+                    {
+                        final int pagePerSec = (int) Math.max(1f, FloatMath.floor(Math.abs(velocityY) / 1000f));
+                        if (DEBUG) Log.d(TAG, String.format("PagesPerVel [%d]", pagePerSec));
+                        if (velocityY < -mMinVelocity)
+                            snapToPage(currPage + pagePerSec, Math.abs(velocityY));
+                        else if (velocityY > mMinVelocity)
+                            snapToPage(currPage - pagePerSec, Math.abs(velocityY));
+                    }
                     // Start scrolling calculation.
-                    mScroller.fling(0, getCurrentScrollY(), 0, (int) -velocityY, 0, 0, nextPageTop - currScrollY, nextPageTop - pageHeight);
+//                    mScroller.fling(0, getCurrentScrollY(), 0, (int) -velocityY, 0, 0, 0, nextPageTop + pageHeight);
 //                    mScroller.startScroll(0, currScrollY, 0, Math.min(lastPageTopY, nextPageTop) - currScrollY, mMinAnimationDuration);
 
                     // Start animation.
                     mScrollView.post(task);
                 }
-
+            case MotionEvent.ACTION_CANCEL:
                 if (mVelocityTracker != null)
                 {
                     mVelocityTracker.recycle();
@@ -140,21 +155,25 @@ public class ScrollViewPager implements View.OnTouchListener
                 }
                 // consume(to stop fling)
                 return true;
-            case MotionEvent.ACTION_CANCEL:
-                break;
         }
         return false;
     }
 
-    void snapToPage(final int whichPage)
+    void snapToPage(int whichPage, final float velocity)
     {
-        final boolean changingPages = whichPage != mCurrentPage;
+        if (whichPage < 0) whichPage = 0;
+        if (whichPage >= getPageCount()) whichPage = getPageCount() - 1;
 
+        final boolean changingPages = whichPage != mCurrentPage;
         mNextPage = whichPage;
 
         final int delta = mContentView.getChildAt(whichPage).getTop() - getCurrentScrollY();
-
-        mScroller.startScroll(0, getCurrentScrollY(), 0, delta, mMinAnimationDuration);
+        int time = mMinAnimationDuration;
+        if (velocity != 0)
+        {
+            time = Math.max(mMinAnimationDuration, (int) ((float) delta / velocity));
+        }
+        mScroller.startScroll(0, getCurrentScrollY(), 0, delta, time);
     }
 
     /**
